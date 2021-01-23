ad449657d7b820403917547dfe39fe9b15eda5a9
MezentsevIlya <ilyamezentcev@gmail.com>
2021-01-23 16:30:35 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+
+def test_function_1(a):
+    a = 100
+    return a
+  
+def test_function_2(a):
+    a = 200
+    return a
+  
+def test_function_1(a):
+    a = 300
+    a *= 2
+    a /= 2
+    a += 100
+    a -= 100
+    return a
+ 
